2baaf37c9867d2fb7826399bae35a40e60279c7f
Rifyal05 <rifyalaidil05@gmail.com>
2025-03-26 14:25:40 +0700

    [Pertemuan 3] Membuat Layout dan Component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,7 +6,7 @@
             <div class="card-body">
                 <h5 class="card-title">PISAU STAINLESS STEEL 18CM</h5>
                 <p class="card-text"> Tingkatkan kesiapan Anda dalam setiap situasi dengan Pisau Stainless Steel! Lebih dari sekadar alat potong biasa, pisau ini dirancang untuk menjadi teman setia dalam berbagai aktivitas, sekaligus memberikan rasa aman tambahan sebagai alat pelindung diri yang efektif. </p>
-                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1130192468960371145%2F&psig=AOvVaw2BkShUxOh_7MFit1Xq-qjV&ust=1743053611525000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKDXse2Cp4wDFQAAAAAdAAAAABAE" class="btn btn-primary">SHOP NOW</a>
+                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tokopedia.com%2Fpd-selamat%2Ftanica-pro-pisau-chef-stainless-steel-02667-pisau-dapur-pisau-daging&psig=AOvVaw3yz11Ha6Xx6FOB9YrvCJdt&ust=1743060273603000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCICNzdWbp4wDFQAAAAAdAAAAABAJ" class="btn btn-primary">SHOP NOW</a>
             </div>
         </div>
     </div>
@@ -18,7 +18,7 @@
             <div class="card-body">
                 <h5 class="card-title">PANCI PENGGORENGAN KAVALKAD</h5>
                 <p class="card-text"> Bukan cuma alat masak, panci ini dibangun kokoh. Gagangnya yang kuat dan ergonomis memberi pegangan mantap saat darurat. Dalam kondisi terdesak, ketahanan dan bobot idealnya bisa jadi alat pertahanan diri yang menambah ketenangan pikiran, terutama jika Anda tinggal sendiri. </p>
-                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1130192468960371145%2F&psig=AOvVaw2BkShUxOh_7MFit1Xq-qjV&ust=1743053611525000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKDXse2Cp4wDFQAAAAAdAAAAABAE" class="btn btn-primary">SHOP NOW</a>
+                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.blibli.com%2Fp%2F1set-2pcs-panci-wajan-penggorengan-alat-masak-kad-merah-original%2Fps--IKF-70007-05083&psig=AOvVaw3ra595qdEvDwFdlDItEOvy&ust=1743060168605000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNCjvaKbp4wDFQAAAAAdAAAAABAL" class="btn btn-primary">SHOP NOW</a>
             </div>
         </div>
     </div>
@@ -30,7 +30,7 @@
             <div class="card-body">
                 <h5 class="card-title">PIRING DAN MANGKUK</h5>
                 <p class="card-text"> Tingkatkan pengalaman bersantap Anda dengan Set Piring & Mangkuk Kaca Premium kami. Didesain tidak hanya untuk memanjakan mata dengan kejernihannya yang indah, tetapi juga untuk memberikan rasa solid dan substansi di tangan Anda. </p>
-                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1130192468960371145%2F&psig=AOvVaw2BkShUxOh_7MFit1Xq-qjV&ust=1743053611525000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKDXse2Cp4wDFQAAAAAdAAAAABAE" class="btn btn-primary">SHOP NOW</a>
+                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.blibli.com%2Fp%2F1set-12pcs-piring-mangkok-makan-kaca-serbaguna-putih-polos-cantik%2Fps--IKF-70007-00571&psig=AOvVaw0G1xeM62M3lnPunhYyR6fF&ust=1743059923497000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCIi5-a2ap4wDFQAAAAAdAAAAABAE" class="btn btn-primary">SHOP NOW</a>
             </div>
         </div>
     </div>
@@ -42,7 +42,7 @@
             <div class="card-body">
                 <h5 class="card-title">SENDOK & GARPU</h5>
                 <p class="card-text">Lupakan sendok & garpu tipis yang mudah bengkok! Perkenalkan Set Garpu & Sendok Stainless Steel Solid kami. dirancang untuk menemani Anda makan dengan nyaman, di mana saja, dan memberikan ketahanan ekstra saat dibutuhkan. </p>
-                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1130192468960371145%2F&psig=AOvVaw2BkShUxOh_7MFit1Xq-qjV&ust=1743053611525000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKDXse2Cp4wDFQAAAAAdAAAAABAE" class="btn btn-primary">SHOP NOW</a>
+                <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tokopedia.com%2Fpd-selamat%2Ftanica-flora-sendok-garpu-set-12-pieces-sendok-makan-set%3Futm_source%3Dgoogle&psig=AOvVaw08QvJbWi0da7KU1yNF7kKQ&ust=1743060108631000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCKCGo4abp4wDFQAAAAAdAAAAABAE" class="btn btn-primary">SHOP NOW</a>
             </div>
         </div>
     </div>
